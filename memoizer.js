441162edc50170c7e4cf16e3b2bddebb636cc714
ViktorFP <VikPr_85@mail.ru>
2016-05-18 23:21:09 +0300

    1.0.2

--- memoizer.js
+++ memoizer.js
@@ -1,40 +1,46 @@
-function factorial(n) {
-    var res = 1;
-    while(n !== 1) {
-        res *= n--;
-	}
-    return res;
-}
-
-var reverseString=function(s){
-	var newStr='';
-	for(var i=s.length-1;i>=0;i--){
-		newStr+=s[i];
+(function(){
+	function factorial(n) {
+		var res = 1;
+		while(n !== 1) {
+			res *= n--;
 		}
-	return newStr;
-};
-
-var MemoizerModule={
-	memoizer:function(func){
-	if (typeof func !== 'function'){		
-      throw 'ArgumentError: function expected';
-	  }
-	var memo={};
-		return function(n){
-			var value;
-			if(n in memo){
-				value = memo[n];
-				}else{
-				value=func.apply(this, arguments);
-				memo[n] = value;
-			}
-			console.log(value);
-			return value;
-		};
+		return res;
 	}
-};
-
-console.log(MemoizerModule.memoizer(factorial)(4));
-
-var str='string';
-console.log('Reverse String \''+str+'\': '+MemoizerModule.memoizer(reverseString)(str));
+	
+	function reverseString(s){
+		var newStr='';
+		for(var i=s.length-1;i>=0;i--){
+			newStr+=s[i];
+		}
+		return newStr;
+	}
+	
+	var MemoizerModule=(function(){
+		function f(func){
+			if (typeof func !== 'function'){		
+				throw 'ArgumentError: function expected';
+			}
+			var memo={};
+			return function(n){
+				var value;
+				if(n in memo){
+					value = memo[n];
+					}else{
+					value=func.apply(this, arguments);
+					memo[n] = value;
+				}
+				console.log(value);
+				return value;
+			};
+		}
+		return {memoizer:f};
+		})();
+	//-------------------------------------------	
+	var n=4;
+	console.log('factorial('+n+'): ');
+	MemoizerModule.memoizer(factorial)(n);
+	
+	var str='string';
+	console.log('Reverse String \''+str+'\': ');
+MemoizerModule.memoizer(reverseString)(str);
+}());
