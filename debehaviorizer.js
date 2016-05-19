8251374aa4aad475a678f197bfdfec956a02bae3
ViktorFP <VikPr_85@mail.ru>
2016-05-19 18:30:08 +0300

    3.0.3

--- debehaviorizer.js
+++ debehaviorizer.js
@@ -3,7 +3,12 @@
 		a:1,
 		b:'str',
 		f:function(){return 'someMethod';},
-		f2:function(){return 'someMethod2';}
+		f2:function(){return 'someMethod2';},
+		ar:['ar1', 'ar2', function(){return 'function in array of obj';}],
+		o:{
+			oa:2,
+			of:function(){return 'method from o';}
+		}
 	};
 	
 	//Object.freeze(obj);
@@ -14,56 +19,83 @@
 	console.log('\nchange current object:');	
 	var array=debehaviorizer(obj, true);
 	console.log('\narray of behavior:');	
-	for(var i=0;i<array.length;i++){
-		console.log(array[i]);
-	}
+	printFunctionArray(array);
 	//example for using (it's for me on future :))
 	console.log(array[0]());
 	//--------------------------
+	function printFunctionArray(arr){
+		for(var i=0;i<arr.length;i++){
+			if(typeof arr[i] === 'object'){
+				printFunctionArray(arr[i]);
+				}else{
+				console.log(arr[i]);
+			}
+		}
+	}
+	
 	function debehaviorizer(obj, flag){
 		function printField(o){
 			for(var prop in o){
-					if(prop){console.log(prop);}
-				}
-				}
-		
-		function behaviorSeparate(){
-			'use strict';
-			var behaviorArray=[], position=0;
-			if(typeof obj === 'object' && obj !== null){			
-				for(var prop in obj){
-					if(typeof obj[prop] === 'function'){
-						behaviorArray[position++]=obj[prop];
-						if(!Object.isFrozen(obj) &&
-						!Object.isSealed(obj)){
-							try{
-								delete obj[prop];
-								continue;
-							}catch(e){}
-						}
-						console.log('Deleting the \''+prop+'\' is impossibly');										
+				if(prop){
+					if((typeof o[prop] === 'object') === false){
+						console.log(prop);
 					}
 				}
-				printField(obj);
+			}
+		}
+		
+		function behaviorSeparate(a){
+			'use strict';
+			var behaviorArray=[], position=0;
+			if(typeof a === 'object' && a !== null){			
+				for(var prop in a){				
+					if(prop){
+						switch(typeof a[prop]){
+							case 'function':
+							behaviorArray[position++]=a[prop];
+							if(!Object.isFrozen(a) &&
+							!Object.isSealed(a)){
+								try{
+									delete a[prop];
+									continue;
+								}catch(e){}
+							}
+							console.log('Deleting the \''+prop+'\' is impossible');		
+							break;
+							case 'object':
+							behaviorArray[position++]=behaviorSeparate(a[prop]);							
+							break;
+						}
+					}
+				}
+				printField(a);
 			}
 		return behaviorArray;}
 		
-		function stateObject(){
+		function stateObject(o){
 			var newObj={};
-			for(var prop in obj){
-			if((typeof obj[prop] === 'function')===false){
-				newObj[prop]=obj[prop];
+			for(var prop in o){
+				if(prop){
+					switch(typeof o[prop]){
+						case 'function':
+						break;
+						case 'object':
+						stateObject(o[prop]);
+						break;
+						default:
+						newObj[prop]=o[prop];
+					}
 				}
 			}
 			printField(newObj);
-			return newObj;}
+		return newObj;}
 		
 		return (function(){
 			if(flag){
-				return behaviorSeparate();
+				return behaviorSeparate(obj);
 				}else{
-				return stateObject();			
+				return stateObject(obj);			
 			}
 		})();
 	}	
-})();
+})();			
